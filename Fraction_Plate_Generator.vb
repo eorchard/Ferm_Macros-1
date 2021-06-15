98f9a3f98124983be046c40d9d76eaf58e5c894b
Zach Goldman <zgoldman@protonmail.com>
2021-06-15 05:57:09 -0700

    Added Quick Populate

--- Fraction_Plate_Generator.vb
+++ Fraction_Plate_Generator.vb
@@ -1,3 +1,159 @@
+'Quickly generates a plate in our typical format
+
+Sub Quick_Populate()
+
+    Dim numberOfVessels As Integer
+
+    Dim DG_Unit As String
+
+    Dim wrapped As Boolean
+
+    Dim counter As Integer
+
+    Dim firstRow As Integer
+
+    counter = 0
+
+    firstRow = 2
+
+ 
+
+    'Collect user input
+
+    numberOfVessels = Range("Y9").Value
+
+    DG_Unit = Range("Y10").Value
+
+    numberOfTimepoints = Range("Y11").Value
+
+   
+
+    'Clear pre-existing data
+
+    Range("C2:E200").ClearContents
+
+   
+
+    'Populate Timepoints
+
+    For x = 0 To numberOfVessels - 1
+
+        For y = 0 To numberOfTimepoints - 1
+
+   
+
+           'If counter reaches 12th column, wrap around
+
+            If counter = 12 And wrapped = False Then
+
+                firstRow = firstRow + 3
+
+                counter = 0
+
+                wrapped = True
+
+               
+
+            'Error notification for excessive samples
+
+            ElseIf counter = 12 And wrapped = True Then
+
+                MsgBox "Plate Overflow! Please adjust Quick Populate settings."
+
+            End If
+
+           
+
+            'Vessel
+
+            Range("C" & (firstRow + (8 * counter))).Value = DG_Unit & x + 1
+
+            Range("C" & (firstRow + 1 + (8 * counter))).Value = DG_Unit & x + 1
+
+            Range("C" & (firstRow + 2 + (8 * counter))).Value = DG_Unit & x + 1
+
+           
+
+            'Timepoint
+
+            Range("D" & (firstRow + (8 * counter))).Value = "I" & Range("Y" & (12 + y)).Value
+
+            Range("D" & (firstRow + 1 + (8 * counter))).Value = "I" & Range("Y" & (12 + y)).Value
+
+            Range("D" & (firstRow + 2 + (8 * counter))).Value = "I" & Range("Y" & (12 + y)).Value
+
+           
+
+            'Color
+
+            Range("E" & (firstRow + (8 * counter))).Value = Range("Y" & (17 + x)).Value
+
+            Range("E" & (firstRow + 1 + (8 * counter))).Value = Range("Y" & (17 + x)).Value
+
+            Range("E" & (firstRow + 2 + (8 * counter))).Value = Range("Y" & (17 + x)).Value
+
+           
+
+            counter = counter + 1
+
+        Next
+
+    Next
+
+   
+
+    'Wrapped state will determine whether CFB ends up on Row D or Row G
+
+    If wrapped = True Then
+
+        firstRow = 8
+
+    Else
+
+        firstRow = 5
+
+    End If
+
+   
+
+    'Populate CFB
+
+    For x = 0 To numberOfVessels - 1
+
+        'Vessel
+
+        Range("C" & firstRow + (8 * x)).Value = DG_Unit & x + 1
+
+        Range("C" & firstRow + 1 + (8 * x)).Value = DG_Unit & x + 1
+
+       
+
+        'Timepoint
+
+        Range("D" & firstRow + (8 * x)).Value = "CFB"
+
+        Range("D" & firstRow + 1 + (8 * x)).Value = "CFB"
+
+       
+
+        'Color
+
+        Range("E" & firstRow + (8 * x)).Value = Range("Y" & (17 + x)).Value
+
+        Range("E" & firstRow + 1 + (8 * x)).Value = Range("Y" & (17 + x)).Value
+
+    Next
+
+   
+
+    Call Generate_Plate
+
+   
+
+End Sub
+
+ 
+
 Sub Generate_Plate()
 
  
@@ -7,6 +163,12 @@ Dim ovalShape As Shape
 Dim A1_X As Double
 
 Dim A1_Y As Double
+
+ 
+
+'Clear plate
+
+Call Clear_Plate
 
  
 
