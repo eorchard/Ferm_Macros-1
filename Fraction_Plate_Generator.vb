f795dfb1476be4cca5e81ff5d04a0988be7a5e11
Zach Goldman <zgoldman@protonmail.com>
2021-06-17 09:26:21 -0700

    Removed double spacing

--- Fraction_Plate_Generator.vb
+++ Fraction_Plate_Generator.vb
@@ -1,333 +1,167 @@
 'Makes string comparisons case insensitive
-
 Option Compare Text
-
  
-
 'Quickly generates a plate in our typical format
-
 Sub Quick_Populate()
-
     Dim numberOfVessels As Integer
-
     Dim DG_Unit As String
-
     Dim wrapped As Boolean
-
     Dim counter As Integer
-
     Dim firstRow As Integer
-
     Dim emptyRightTwo As Boolean
-
     Dim exitFor As Boolean
-
     counter = 0
-
     firstRow = 2
-
  
-
     'Collect user input
-
     numberOfVessels = Range("Y9").Value
-
     DG_Unit = Range("Y10").Value
-
     numberOfTimepoints = Range("Y11").Value
-
     emptyRightTwo = IIf(Range("Y26").Value = "Yes", True, False)
-
    
-
     Call Clear_Table
-
    
-
     'Populate Timepoints
-
     For x = 0 To numberOfVessels - 1
-
         For y = 0 To numberOfTimepoints - 1
-
    
-
            'If counter reaches 12th column, wrap around
-
             If counter = 12 And Not wrapped Then
-
                 firstRow = firstRow + 3
-
                 counter = 0
-
                 wrapped = True
-
                
-
             ElseIf counter = 10 And Not wrapped And emptyRightTwo Then
-
                 firstRow = firstRow + 3
-
                 counter = 0
-
                 wrapped = True
-
                
-
             'Error notification for excessive samples
-
             ElseIf (counter = 12 And wrapped) Or (counter = 10 And wrapped And emptyRightTwo) Then
-
                 MsgBox "Plate Overflow! Please adjust Quick Populate settings."
-
                 'Break loop
-
                 exitFor = True
-
                 Exit For
-
             End If
-
            
-
             'Vessel
-
             Range("C" & (firstRow + (8 * counter))).Value = DG_Unit & x + 1
-
             Range("C" & (firstRow + 1 + (8 * counter))).Value = DG_Unit & x + 1
-
             Range("C" & (firstRow + 2 + (8 * counter))).Value = DG_Unit & x + 1
-
            
-
             'Timepoint
-
             Range("D" & (firstRow + (8 * counter))).Value = "I" & Range("Y" & (12 + y)).Value
-
             Range("D" & (firstRow + 1 + (8 * counter))).Value = "I" & Range("Y" & (12 + y)).Value
-
             Range("D" & (firstRow + 2 + (8 * counter))).Value = "I" & Range("Y" & (12 + y)).Value
-
            
-
             'Color
-
             Range("E" & (firstRow + (8 * counter))).Value = Range("Y" & (18 + x)).Value
-
             Range("E" & (firstRow + 1 + (8 * counter))).Value = Range("Y" & (18 + x)).Value
-
             Range("E" & (firstRow + 2 + (8 * counter))).Value = Range("Y" & (18 + x)).Value
-
            
-
             counter = counter + 1
-
         Next
-
         'Break loop
-
         If exitFor Then Exit For
-
     Next
-
    
-
     'Wrapped state will determine whether CFB ends up on Row D or Row G
-
     firstRow = IIf(wrapped, 8, 5)
-
    
-
     'Populate CFB
-
     For x = 0 To numberOfVessels - 1
-
         'Vessel
-
         Range("C" & firstRow + (8 * x)).Value = DG_Unit & x + 1
-
         Range("C" & firstRow + 1 + (8 * x)).Value = DG_Unit & x + 1
-
        
-
         'Timepoint
-
         Range("D" & firstRow + (8 * x)).Value = "CFB"
-
         Range("D" & firstRow + 1 + (8 * x)).Value = "CFB"
-
        
-
         'Color
-
         Range("E" & firstRow + (8 * x)).Value = Range("Y" & (18 + x)).Value
-
         Range("E" & firstRow + 1 + (8 * x)).Value = Range("Y" & (18 + x)).Value
-
     Next
-
    
-
     Call Generate_Plate
-
    
-
 End Sub
-
  
-
 Sub Generate_Plate()
-
  
-
 Dim ovalShape As Shape
-
 Dim A1_X As Double
-
 Dim A1_Y As Double
-
  
-
 'Clear plate
-
 Call Clear_Plate
-
  
-
 'Absolute location of well A1, rest will be calculated relatively
-
 Set plateLocation = ActiveSheet.Shapes("Plate Map")
-
 A1_X = 397.5
-
 A1_Y = 60.75
-
  
-
 'Initialize plate map in correct location with correct size
-
 With ActiveSheet.Shapes("Plate Map")
-
     .Left = 366
-
     .Top = 1.25
-
     .Height = 378.72
-
     .Width = 510.48
-
 End With
-
  
-
 'Populate arrays with values, begin at x+1 since the first row is a header
-
 Dim colorArray(96) As String
-
 Dim timepointArray(96) As String
-
 Dim dasgipArray(96) As String
-
 Dim x As Integer
-
  
-
 For x = 1 To 96
-
     dasgipArray(x) = Cells(x + 1, 3).Value
-
     timepointArray(x) = Cells(x + 1, 4).Value
-
     colorArray(x) = Cells(x + 1, 5).Value
-
 Next
-
    
-
 'Populate wells
-
 Dim color As String
-
 Dim counter As Integer
-
 counter = 1
-
  
-
 For i = 1 To 12
-
     For j = 1 To 8
-
         color = colorArray(counter)
-
        
-
         'Skip elements in colorArray that are empty
-
         If colorArray(counter) <> "" Then
-
             Set ovalShape = ActiveSheet.Shapes(color).Duplicate
-
             With ovalShape
-
                 .Name = color & "_Copy"
-
                 .Left = A1_X
-
                 .Top = A1_Y
-
                
-
                 'Color wells relative to A1
-
                 .IncrementLeft 38.75 * (i - 1)
-
                 .IncrementTop 38.25 * (j - 1)
-
                
-
                 'Populate wells with text
-
                 With .TextFrame
-
                     .MarginLeft = 0
-
                     .MarginRight = 0
-
                     .MarginTop = 0
-
                     .MarginBottom = 0
-
                     .Characters.Text = dasgipArray(counter) & vbNewLine & timepointArray(counter)
-
                     .Characters.Font.Size = 9
-
                     .Characters.Font.Bold = True
-
                     .HorizontalAlignment = xlHAlignCenter
-
                 End With
-
             End With
-
         End If
-
         counter = counter + 1
-
     Next
-
 Next
-
    
-
 End Sub
-
  
-
 'Clear pre-existing data
-
 Sub Clear_Table()
-
     Range("C2:E200").ClearContents
-
 End Sub
--- JMP_Macro.vb
+++ JMP_Macro.vb
@@ -1,370 +1,184 @@
-
-
 'Function counts number of DO spikes based on Pump A totalizer values
-
 Function countDOSpikes(lastRow, sourceSheet) As Integer
-
     Dim numberOfSpikes As Integer
-
     Dim highTotalizer As Double
-
     Dim potentialHighTotalizer As Double
-
     Dim timeArray As Variant
-
  
-
     numberOfSpikes = 0
-
     highTotalizer = 0
-
     potentialHighTotalizer = 0
-
  
-
     For Each cell In sourceSheet.Range("AF2:AF" & lastRow)
-
         'Skip empty cells
-
         If cell.Value <> "" Then
-
             'Identifying DO spikes will require at least a 10mL increase
-
             If (cell.Value > highTotalizer + 10) Then
-
                 'potentialHighTotalizer becomes highTotalizer if new totalizer value appears in raw data 3x consecutively
-
                 'This prevents the macro from identifying a gradual increase as multiple spikes
-
                 If (cell.Value = potentialHighTotalizer) And (counter = 3) Then
-
                     highTotalizer = potentialHighTotalizer
-
                     counter = 0
-
                     numberOfSpikes = numberOfSpikes + 1
-
                    
-
                 ElseIf (cell.Value = potentialHighTotalizer) And (counter < 3) Then
-
                     counter = counter + 1
-
                    
-
                 ElseIf cell.Value <> potentialHighTotalizer Then
-
                     potentialHighTotalizer = cell.Value
-
                 End If
-
             End If
-
         End If
-
     Next
-
    
-
     countDOSpikes = numberOfSpikes
-
 End Function
-
  
-
 'Sub adds DG Column to raw data
-
 Private Sub addDGColumn(DG_Unit, vesselNumber)
-
     Columns("C:C").Select
-
     Selection.End(xlDown).Select
-
     Range("A2:A" & ActiveCell.Row - 1).EntireRow.Delete
-
     ActiveWindow.ScrollRow = 1
-
     Range("C1").EntireColumn.Insert
-
     Range("C1").Value = "DG"
-
     Range("C2:C" & Range("B2").End(xlDown).Row).Value = DG_Unit & vesselNumber
-
 End Sub
-
  
-
 'Function determines number of DG units based on number of data sheets
-
 Function countDataSheets(countFromWorkbook) As Integer
-
     Dim i As Integer
-
     Dim xCount As Integer
-
    
-
     'Count number of sheets containing keyword 'Data'
-
     For i = 1 To countFromWorkbook.Sheets.Count
-
         If InStr(1, countFromWorkbook.Sheets(i).Name, "Data") > 0 Then xCount = xCount + 1
-
     Next
-
    
-
     'Minus 1 since the DASGIP raw data file has an extra blank sheet named 'Data'
-
     countDataSheets = xCount - 1
-
 End Function
-
  
-
 'Function removes timepoints before inoculation time, populates a new column with name of DG unit
-
 Private Sub compressData(numberOfDataSheets, DG_Unit)
-
     For i = 1 To numberOfDataSheets
-
         Sheets("Data" & i).Select
-
        
-
         'Need this condition to check if Data Sheet # matches actual DG unit since the vessels aren't always run in sequence
-
         If Range("E1").Value Like "*" & i & "*" Then
-
             Call addDGColumn(DG_Unit, i)
-
            
-
         'If DG vessel doesn't match up with Data Sheet #, compare the other numbers
-
         Else
-
             For j = 1 To 8
-
                 If Range("E1").Value Like "*" & j & "*" Then
-
                     Call addDGColumn(DG_Unit, j)
-
                 End If
-
             Next
-
         End If
-
      Next
-
 End Sub
-
  
-
 'Function will consolidate data onto one sheet
-
 Private Sub consolidateData(numberOfDataSheets)
-
     If (numberOfDataSheets > 1) Then
-
         For i = 2 To numberOfDataSheets
-
             Sheets("Data" & i).Select
-
             Range("A2:AO" & Range("B2").End(xlDown).Row).Copy
-
             Sheets("Data1").Select
-
             Columns("A:A").Select
-
             Selection.End(xlDown).Offset(1, 0).Select
-
             ActiveSheet.Paste
-
         Next
-
     End If
-
    
-
     'Remove number "1" from headers
-
     Worksheets("Data1").Rows("1").Replace What:="1", Replacement:=""
-
 End Sub
-
  
-
 'Function imports raw data file from DG units
-
 Private Sub importRawData()
-
     Dim filter As String
-
     Dim rawDataFilename As String
-
     Dim rawDataWorkbook As Workbook
-
     Dim targetWorkbook As Workbook
-
     Dim numberOfSpikes As Integer
-
     Dim DG_Unit As String
-
    
-
     filter = "Text files (*.xlsx),*.xlsx"
-
     MsgBox "Please select the DASGIP raw data file", vbOKOnly
-
    
-
     'JMP Macro workbook is the target
-
     Set targetWorkbook = Application.ThisWorkbook
-
    
-
     'Get raw data workbook
-
     rawDataFilename = Application.GetOpenFilename(filter, , caption)
-
     Set rawDataWorkbook = Application.Workbooks.Open(rawDataFilename)
-
    
-
     'This function makes the macro compatible with any number of DG vessels
-
     numberOfDataSheets = countDataSheets(rawDataWorkbook)
-
    
-
     'Identify which DASGIP the raw data is coming from, currently relies on filename
-
     If rawDataWorkbook.Name Like "*" & "DG3" & "*" Then
-
         DG_Unit = "DG3_u"
-
     ElseIf rawDataWorkbook.Name Like "*" & "DG4" & "*" Then
-
         DG_Unit = "DG4_u"
-
     ElseIf rawDataWorkbook.Name Like "*" & "DG5" & "*" Then
-
         DG_Unit = "DG5_u"
-
     End If
-
    
-
     'Remove timepoints before inoculation from raw data
-
     Call compressData(numberOfDataSheets, DG_Unit)
-
    
-
     'Copy data from DG raw files to JMP Macro
-
     For i = 1 To numberOfDataSheets
-
         Dim targetSheet As Worksheet
-
         Set targetSheet = targetWorkbook.Worksheets("Data" & i)
-
         Dim sourceSheet As Worksheet
-
         Set sourceSheet = rawDataWorkbook.Worksheets("Data" & i)
-
        
-
         'Identify last row in order to extract the correct range
-
         Dim lastRow As Long
-
         lastRow = Application.WorksheetFunction.CountA(Columns(1))
-
        
-
         'DG3 and DG5 raw data export contain 6 additional columns than DG4, remove these columns so all DG units are formatted the same way
-
         If Application.WorksheetFunction.CountA(sourceSheet.Range("AN:AN")) <> 0 Then
-
             sourceSheet.Range("J:J,P:P,R:R,T:T,AL:AL,AN:AN").Delete
-
         End If
-
        
-
         targetSheet.Range("A2", "AI" & lastRow).Value = sourceSheet.Range("A2", "AI" & lastRow).Value
-
        
-
         numberOfSpikes = countDOSpikes(lastRow, sourceSheet)
-
     Next
-
    
-
     'Close raw data file
-
     rawDataWorkbook.Close SaveChanges:=False
-
    
-
     'Append all DG raw data to bottom of first sheet
-
     Call consolidateData(numberOfDataSheets)
-
    
-
     'Convert Duration and InoculationTime to "hh:mm:ss" format
-
     lastRow = Application.WorksheetFunction.CountA(Columns(1))
-
    
-
     'Duration
-
     For Each cell In Range("B2:B" & lastRow)
-
         cell.Value = "=TEXT(""" & cell.Value & """, ""hh:mm:ss"")"
-
     Next cell
-
    
-
     'InoculationTime
-
     For Each cell In Range("D2:D" & lastRow)
-
         cell.Value = "=TEXT(""" & cell.Value & """, ""hh:mm:ss"")"
-
     Next cell
-
    
-
 End Sub
-
  
-
 'Main macro container
-
 Sub Run_JMP_Macro()
-
     'Clear pre-existing data
-
     For i = 1 To 8
-
         Worksheets(i).Range("A2:AI" & Rows.Count).ClearContents
-
     Next
-
    
-
     'Import DG raw data file
-
     Call importRawData
-
 End Sub
-
