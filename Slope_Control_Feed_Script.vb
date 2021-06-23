9360e1e2d3cac7ed9617806fd19caedf0cee84dc
Zach Goldman <zgoldman@protonmail.com>
2021-06-23 05:40:19 -0700

    Added comments

--- Slope_Control_Feed_Script.vb
+++ Slope_Control_Feed_Script.vb
@@ -6,7 +6,7 @@
   dim DO_low_trigger as double = 30                 '[%] usually DO setpoint
   dim DO_high_trigger as double = 40                '[%] feed trigger
   dim bolus_size as double = 30                     '[g/L]
-  dim Wait_Before_DO_Detection as double = 1/60    '[h] blind time time before looking for high DO trigger after hitting low trigger
+  dim Wait_Before_DO_Detection as double = 15/60    '[h] blind time time before looking for high DO trigger after hitting low trigger
 
 '### These parameters should remain static generally ###
   dim Wait_Before_Feed_Start as double = 1/60       '[h] Time before high trigger and feeding should be 0.033 for 2min delay - to prevent false alarms
@@ -29,8 +29,10 @@ if p isnot nothing then
 
       case 1
         if .InoculationStart_H > 0 then
+          'Reset totalizer values
           .SetVAPV(0)
           .SetVBPV(0)
+
           .phase = .phase + 1
           .LogMessage("Entering phase: Totalizer values reset. Waiting for DO falling under " & DO_low_trigger & "%")
         end if
@@ -69,15 +71,21 @@ if p isnot nothing then
         DO_Slope = ((DO_high_trigger/100)-(DO_low_trigger/100))/(.Runtime_H - .PhaseStart_H)
 
         if .DOPV > DO_high_trigger then
+
+          'Check for minimum slope 
           if ((DO_Slope > Minimum_Slope_For_Feed) Or (.ExtA < 1)) then
             .phase = .phase + 1
             .LogMessage("Entering phase: Waiting for high DO longer than " & Wait_Before_Feed_Start & "h")
+
+          'Slope is below the minimum, don't feed
           else
             .phase = .phase - 3
             .LogMessage("Slope of " & DO_Slope & " doesn't meet minimum slope of " & Minimum_Slope_For_Feed)
             .LogMessage("Entering phase: Waiting for DO falling under " & DO_low_trigger & "%")
           end if
         end if
+
+        'Slope falls back below low trigger
         if .DOPV < DO_low_trigger then
           .phase = .phase - 2
           .LogMessage("Entering phase: Waiting for DO > "& DO_high_trigger &"%")
@@ -88,11 +96,14 @@ if p isnot nothing then
           .phase = .phase - 1
           .LogMessage("Entering phase: Waiting for DO > "& DO_high_trigger &"%")
         end if 
+
         if .Runtime_H - .PhaseStart_H > Wait_Before_Feed_Start then
             .phase = .phase + 1
             .LogMessage("Entering phase: Starting feeds A")
             .FASP = Feed_Rate
-            .ExtA = .ExtA + 1     'this indicates number of DO spikes, currently used to identify Citric Acid spike but may be useful in a future script
+
+            'Count number of DO spikes, currently used to identify Citric Acid spike but may be useful in a future script
+            .ExtA = .ExtA + 1     
         end if
 
       case 7
